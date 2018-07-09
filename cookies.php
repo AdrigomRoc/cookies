1faa842e9fd300a42455d8ffda7e83828c78cf42
AdrigomRoc <adrigomroc@gmail.com>
2018-07-09 09:30:12 +0200

    Botó d'esborrar cookies

--- cookies.php
+++ cookies.php
@@ -3,24 +3,39 @@
 <body>
     
     <?php
-        if(isset($_POST["email"])){
-            $email = $_POST["email"];
-        }
-        if(isset($_POST["password"])){
-            $password = $_POST["password"];
+    $cookie_name="guardapas";
+    
+        if(isset($_POST["type"])){
+            if($_POST["type"] === "create"){
+                if(isset($_POST["email"])){
+                    $email = $_POST["email"];
+                }
+                if(isset($_POST["password"])){
+                    $password = $_POST["password"];
+                }
+    
+                if(isset($password) && $password=="test"){
+                    echo "Email: $email , Password: $password";
+                    setcookie($cookie_name, $password, time() + (86400 * 30), "/");
+                    echo "COOKIE CREADA";
+                }
+
+            }else if($_POST["type"] === "delete"){
+                if(isset($_COOKIE[$cookie_name])){
+                    setcookie($cookie_name, null, -1, '/');
+                    echo "COOKIE ELIMINADA";
+                }
+            }
+        
         }
 
-        $cookie_name="guardapas";
-        if(isset($password) && $password=="test"){
-            echo "Email: $email , Password: $password";
-            setcookie($cookie_name, $password, time() + (86400 * 30), "/");
-        }
         if(isset($_COOKIE[$cookie_name])){
             echo "Cookie existeix!";
         }
         //var_dump($_COOKIE);
     ?>
     <form action="cookies.php" method="post">
+        <input type="hidden" name="type" value="create">
         Correu: 
         <input type="text" name="email">
         <br />
@@ -28,15 +43,13 @@
         <input type="password" name="password">
         <br />
         <input type="submit">
+        <br/>
     </form>
-    <?php
-    $array = array();
-    $array["Adri"] = 10;
-    $array["Aitor"] = 20;
-    $array["Alex"] = 30;
-    
-    //var_dump($array);
-
-?>
+    <hr/>
+    <form action="cookies.php" method="post">
+        <input type="hidden" name="type" value="delete">
+        <input type="submit" value="Logout">
+        <br/>
+    </form>
 </body>
 </html>
